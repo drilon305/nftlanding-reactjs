32c226408ef806d72339afb3518112c833cb7fdc
Drilon <navycrack1@gmail.com>
2022-02-27 19:47:17 +0100

    releases section

--- src/components/Clients.js
+++ src/components/Clients.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import clients1 from "../assets/clients1.png";
+import clients2 from "../assets/clients2.png";
+import clients3 from "../assets/clients3.png";
+import clients4 from "../assets/clients4.png";
+import clients5 from "../assets/clients5.png";
 
 const Clients = () => {
-  return (
-    <div>Clients</div>
-  )
-}
+  const data = [clients1, clients2, clients3, clients4, clients5];
 
-export default Clients
+  return (
+    <div className="clients">
+      <div className="container">
+        <div className="clients-container">
+          {data.map((client, i) => (
+            <div className="client" key={i}>
+              <img src={client} alt="client" />
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Clients;
