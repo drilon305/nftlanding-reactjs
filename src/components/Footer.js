72105bd91871d0ddf8f3aab763b3968af2b4a2f4
Drilon <navycrack1@gmail.com>
2022-02-28 20:15:13 +0100

    completed footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,73 @@
 import React from 'react'
+import logo from "../assets/logo.png";
+import { BsFacebook, BsTwitter, BsInstagram } from "react-icons/bs";
+import { FaTiktok } from "react-icons/fa";
+
+  
 
 const Footer = () => {
+  const links = [
+    {
+      title: "About",
+      data: ["About", "Terms", "Legal"],
+    },
+    {
+      title: "NFT",
+      data: ["OpenSea", "Maker", "Learn"],
+    },
+    {
+      title: "Contact",
+      data: ["Press", "Support"],
+    },
+    {
+      title: "Social",
+      data: ["Twiiter", "Instagram"],
+    },
+  ];
+  const socialLink = [
+    <BsFacebook />,
+    <BsTwitter />,
+    <BsInstagram />,
+    <FaTiktok />,
+  ];
+
   return (
-    <div>Footer</div>
+    <footer>
+      <div className="upper">
+        <div className="brand-container">
+          <div className="brand">
+            <img src={logo} alt='logo' />
+          </div>
+          <p>Exclusive NFT Collection</p>
+          <ul>
+            {socialLink.map((link, i) => (
+              <li key={i}>
+                {link}
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="links">
+          {links.map(({title, data}, i) => {
+            return (
+              <div className="link" key={i}>
+                <h4>{title}</h4>
+                <ul>
+                {data.map((link, index) => (
+              <li key={index}>
+                {link}
+              </li>
+            ))}
+                </ul>
+              </div>
+            )
+          })}
+        </div>
+      </div>
+      <div className="lower">
+        <span>&copy; Copyright 2022 NFT</span>
+      </div>
+    </footer>
   )
 }
 
