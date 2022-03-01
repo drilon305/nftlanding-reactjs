bb92829ff4510c82aa8a01e4d50c28aaf021cb5f
Drilon <navycrack1@gmail.com>
2022-03-01 23:34:58 +0100

    scroll to top

--- src/components/ScrollToTop.js
+++ src/components/ScrollToTop.js
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { BsChevronUp } from 'react-icons/bs'
 
 const ScrollToTop = () => {
+  const [visible, setVisible] = useState(false)
+  window.addEventListener('scroll', () => {
+    window.pageYOffset > 100 ? setVisible(true) : setVisible(false)
+  })
+
   return (
-    <div>ScrollToTop</div>
+    <div className={`scrollTop ${visible ? 'visible' : ''}`}>
+      <a href="#">
+        <BsChevronUp />
+      </a>
+    </div>
   )
 }
 
