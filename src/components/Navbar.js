29d82322413aa03378936b71823635a90e7256da
Drilon <navycrack1@gmail.com>
2022-03-04 22:51:56 +0100

    finished my project and ready for deploy

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,30 +8,52 @@ import logo from "../assets/logo.png";
 export default function Navbar({ changeTheme, currentTheme }) {
   const [navState, setNavState] = useState(false)
 
-  return <nav>
-    <div className="brand-container">
-      <div className="brand">
-        <img src={logo} alt='logo' />
-      </div>
-      <div className="toggle-container">
-        <div className="toggle">
-          {navState ? <MdClose onClick={() => setNavState(false)} /> : <GiHamburgerMenu onClick={() => setNavState(true)} />}
+  return (
+    <nav>
+      <div className="brand-container">
+        <div className="brand">
+          <img src={logo} alt="logo" />
+        </div>
+        <div className="toggle-container">
+          <div className="toggle">
+            {navState ? (
+              <MdClose onClick={() => setNavState(false)} />
+            ) : (
+              <GiHamburgerMenu onClick={() => setNavState(true)} />
+            )}
           </div>
-        <div className="mode">
-          {currentTheme === 'dark' ? <ImSun className="light" /> : <BsFillMoonFill className="dark" />}
+          <div className="mode" onClick={changeTheme}>
+            {currentTheme === "dark" ? (
+              <ImSun className="light" />
+            ) : (
+              <BsFillMoonFill className="dark" />
+            )}
+          </div>
         </div>
       </div>
-    </div>
-    <div className={`links-container ${navState ? "nav-visible" : ""}`}>
-      <ul className="links">
-        <li><a href="#">Features</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Launch</a></li>
-        <li><a href="#">SignUp</a></li>
-        <li onClick={changeTheme}>
-          {currentTheme === 'dark' ? <ImSun className="light" /> : <BsFillMoonFill className="dark" />}
-        </li>
-      </ul>
-    </div>
-  </nav>
+      <div className={`links-container ${navState ? "nav-visible" : ""}`}>
+        <ul className="links">
+          <li>
+            <a href="#">Features</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Launch</a>
+          </li>
+          <li>
+            <a href="#">SignUp</a>
+          </li>
+          <li onClick={changeTheme}>
+            {currentTheme === "dark" ? (
+              <ImSun className="light" />
+            ) : (
+              <BsFillMoonFill className="dark" />
+            )}
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
